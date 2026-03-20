448569dd63086cf4006cbb1cb22219acac51639b
Islam Ali <islamali2004111@gmail.com>
2026-03-20 18:25:44 +0200

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -4,7 +4,7 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         while(true){
-            Console.WriteLine("$ ");
+            Console.Write("$ ");
             string command = String.Empty;
             command = Console.ReadLine();
             Console.WriteLine($"{command}: command not found");
