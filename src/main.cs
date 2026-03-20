0fa28eb61fd860095246641196329badeab62e4f
Islam Ali <islamali2004111@gmail.com>
2026-03-20 18:40:30 +0200

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -15,6 +15,18 @@ class Program
                 command = command.Substring(5);
                 Console.WriteLine(command);
                 continue;
+            }else if (command.StartsWith("type"))
+            {
+                command = command.Substring(5);
+                if (command == "type" || command == "exit" || command == "echo")
+                {
+                    Console.WriteLine($"{command} is a shell builtin");
+                }
+                else
+                {
+                    Console.WriteLine($"{command}: not found");
+                }
+                continue;
             }
             Console.WriteLine($"{command}: command not found");
         }
