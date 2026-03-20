f2db358614ddb6bfcec16ac52d7d6cea37f4bb65
Islam Ali <islamali2004111@gmail.com>
2026-03-20 18:14:18 +0200

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -3,6 +3,9 @@ class Program
     static void Main()
     {
         // TODO: Uncomment the code below to pass the first stage
-         Console.Write("$ ");
+        Console.Write("$ ");
+        string command = Console.ReadLine();
+        Console.Write($"{command}: command not found");
+
     }
 }
